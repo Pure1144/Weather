7a95b727a65cac2a3bb6e6600ac4fc43371b8a0f
Pure1144 <purevdorj@enlight.mn>
2025-01-16 20:13:18 +0800

    2025011602

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -3,8 +3,8 @@ export const WeatherInfo = (props) => {
     console.log(filteredData);
     
   return (
-    <div className="w-screen h-screen flex relative  justify-center items-center">
-               <div className="flex absolute justify-center items-center top-10 right-10 flex-col">
+    <div className="w-screen h-screen flex relative  justify-center top-10 left-1">
+               <div className="flex absolute justify-center items-center ">
                <input className=" border border-black h-[40px] w-[100px] " value={searchValue} onChange={onChange} />
                <div className="w-[300px] ">
                {filteredData.map((el)=> 
@@ -14,12 +14,14 @@ export const WeatherInfo = (props) => {
                </div>
 
 
-      <div className="w-1/2 h-full flex justify-center items-center">
+      <div className="w-1/2 h-full flex justify-center ">card0
 
-        <div className="w-[100px] h-[200px] bg-orange-300">card</div>
+        <div className="w-[600px] h-[900px] bg-orange-300 flex">card1
+        <div className="w-[500px] h-[700px] bg-blue-300 fit-content">card2</div>
+        </div>
       </div>
       <div className="w-1/2 h-full flex justify-center items-center">
-        <div className="w-[100px] h-[200px] bg-orange-300">card</div>
+        <div className="w-[600px] h-[900px] bg-orange-300">card</div>
       </div>
 
     </div>
